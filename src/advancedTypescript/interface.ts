b98dc6e1461e282c2ab129ce3e7f2cf12bfc1555
Sumon <m.sumon@outlook.com>
2023-05-06 01:48:17 +0600

    uses of interface and type alias added

--- src/advancedTypescript/interface.ts
+++ src/advancedTypescript/interface.ts
@@ -29,4 +29,22 @@ const user2: IExtendedUser = {
     name: 'Omanush',
     age: 200,
     role: "unknown"
+};
+// type alias with function
+type addNumberType = (num1: number, num2: number) => number;
+const addNumber: addNumberType = (num1, num2) => num1 + num2;
+
+// interface with function 
+interface IAddNumber {
+    (num1: number, num2: number): number;
 }
+
+// interface with array 
+interface IRollNumbers {
+    [index: number]: string;
+};
+const rollNumbers: IRollNumbers = ['4', '2,', "1"];
+
+// type alias with array 
+type rollNumberType = number[];
+const rollNumber: rollNumberType = [1, 2, 3]
