d71caf64237fdde0f53888a999ccac9e6e896521
Sumon <m.sumon@outlook.com>
2023-05-08 02:29:30 +0600

    inheritance added

--- src/advancedTypescript/generic-interface.ts
+++ src/advancedTypescript/generic-interface.ts
@@ -1,42 +1,44 @@
-// Generic interface 
+// Generic interface
 interface CrushInterface<T, U = null> {
-    name: string,
-    husband: T;
-    wife?: U 
+  name: string;
+  husband: T;
+  wife?: U;
 }
 const crush1: CrushInterface<boolean, string> = {
-    name: 'Kate Winslet',
-    husband: true,
-    wife: "Sokina"
+  name: "Kate Winslet",
+  husband: true,
+  wife: "Sokina",
 };
 const crush2: CrushInterface<string> = {
-    name: 'Katty Perry',
-    husband: "Persian"
-}
+  name: "Katty Perry",
+  husband: "Persia",
+};
 
 //interface with object with multiple parameter in interface
-interface HusbandInterface { name: string, salary: number }
-const crush3: CrushInterface<HusbandInterface> = {
-    name: "Leonerdo Dikaprio",
-    husband: {
-        name: "Leonardo",
-        salary: 2000
-    }
+interface HusbandInterface {
+  name: string;
+  salary: number;
 }
-
+const crush3: CrushInterface<HusbandInterface> = {
+  name: "Leonerdo Dikaprio",
+  husband: {
+    name: "Leonardo",
+    salary: 2000,
+  },
+};
 
 interface PersonInterface {
-    name: string,
-    age: number
+  name: string;
+  age: number;
 }
 const cursh4: CrushInterface<PersonInterface, PersonInterface> = {
-    name: "Apu Biswas",
-    husband: {
-        name: "Sakib Khan",
-        age: 40,
-    },
-    wife: {
-        name: "Bubly",
-        age: 32
-    }
-}
+  name: "Apu Biswas",
+  husband: {
+    name: "Sakib Khan",
+    age: 40,
+  },
+  wife: {
+    name: "Bubly",
+    age: 32,
+  },
+};
